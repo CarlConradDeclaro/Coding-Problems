cbafc94b21853444ef22bce1eceba0a76aea78b8
carl conrad <carlconrad98071234@gmail.com>
2023-09-17 21:54:22 +0800

    descending order

---
+++ 7kyu/Descending Order/descendingOrder.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+ class descendingOrder{
+     
+    public static int sortDesc(final int num) {
+
+         String n = String.valueOf(num);
+         char arr[] = new char[n.length()];
+         
+         for (int i = 0; i <n.length(); i++) {
+            arr[i] = n.charAt(i );
+         }       
+         Arrays.sort(arr);
+         String s = new StringBuilder(String.valueOf(arr)).reverse().toString();  
+         return Integer.valueOf(s);
+    }
+
+     public static void main(String[] args) {
+        System.out.println(sortDesc(32145));    
+     }
+ }
